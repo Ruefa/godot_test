d3d192dc01fe73e89b620e370c3961bba152a9fc
ruefa <ruef.alex@gmail.com>
2019-04-24 16:33:16 -0700

    added bullet movement

--- Side Scroll/Bullet.gd
+++ Side Scroll/Bullet.gd
@@ -7,8 +7,11 @@ func _ready():
 	add_collision_exception_with($Player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	move_and_slide(Vector2(SPEED,0), Vector2(0,-1))
+	
+	if get_slide_count() > 0:
+		queue_free()
 
 
 func _on_DespawnTimer_timeout():
