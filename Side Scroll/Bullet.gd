41aee415f895b19716f53141ed12fba3d50cd26f
ruefa <ruef.alex@gmail.com>
2019-04-24 16:30:36 -0700

    added collision exption for bullet with the player

--- Side Scroll/Bullet.gd
+++ Side Scroll/Bullet.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	add_collision_exception_with($Player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
