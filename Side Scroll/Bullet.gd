265e92ca3a1ba0aa43bdc41375b6f242d5a12227
ruefa <ruef.alex@gmail.com>
2019-04-25 14:32:41 -0700

    excluding collision with player wasn't working and is no longer needed

--- Side Scroll/Bullet.gd
+++ Side Scroll/Bullet.gd
@@ -5,7 +5,7 @@ var velocity = Vector2(SPEED, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_collision_exception_with($Player)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
