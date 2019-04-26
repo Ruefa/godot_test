b6bde7b1d4433decb9aa1269c26b3ffb996ff359
ruefa <ruef.alex@gmail.com>
2019-04-25 17:51:23 -0700

    moved menu pause control to main

--- Side Scroll/Main.gd
+++ Side Scroll/Main.gd
@@ -7,8 +7,15 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	# pause menu
+	if Input.is_action_pressed("ui_menu"):
+		if get_tree().paused:
+			get_tree().paused = false
+			$Menu.hide()
+		else:
+			get_tree().paused = true
+			$Menu.show()
 
 
 func _on_MobSpawnTimer_timeout():
