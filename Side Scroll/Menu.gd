43d6e930112aad6b2bd42a370bb429455c1feaa6
ruefa <ruef.alex@gmail.com>
2019-04-29 19:41:43 -0700

    changed menu to use allocating skill function

--- Side Scroll/Menu.gd
+++ Side Scroll/Menu.gd
@@ -19,5 +19,9 @@ func _on_Damage_pressed():
 
 
 func _on_AttSpeed_pressed():
-	player.att_speed_mult -= .1
-	player.statChange()
+	var skills = player.get_node("Skills")
+	if skills.skillPoints > 0:
+		skills.allocateSkill("attSpeed", -.1)
+		player.statChange()
+	else:
+		print("no skill points")
