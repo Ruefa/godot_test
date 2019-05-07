94a30febe3e48d9e1d86867bce4aca1611b5cdf3
ruefa <ruef.alex@gmail.com>
2019-05-07 15:42:29 -0700

    added comments to skill tree generation

--- Side Scroll/Menu.gd
+++ Side Scroll/Menu.gd
@@ -29,13 +29,17 @@ func _on_AttSpeed_pressed():
 	else:
 		print("no skill points")
 		
+
+# generate skill tree from JSON file
 func createSkillTree():
 	var treeFile = File.new()
 	treeFile.open("res://JSON/skill_tree.json", treeFile.READ)
 	
+	# parse json into text map
 	var treeDict = JSON.parse(treeFile.get_as_text()).result
 	treeFile.close()
 	
+	# iterate through each skill and create a button for the player to click
 	for skill in treeDict:
 		var skillNode = Skill_Node.instance()
 		skillNode.type = skill["type"]
