ae2a179c8f328389074f7b870a663c2a88114e30
ruefa <ruef.alex@gmail.com>
2019-05-10 13:20:29 -0700

    changed Skills to use constant value and started saving skills in a dict

--- Side Scroll/Skills.gd
+++ Side Scroll/Skills.gd
@@ -1,14 +1,20 @@
 extends Node
 
+var consts = load("res://Constants.gd")
+
 var skillPoints = 5
 
-var att_speed_mult = 0
+var stats = {
+	consts.ATT_SPEED : 0,
+	consts.ATT_DAMAGE : 0
+}
 
 #func _ready():
 
 func allocateSkill(type, value):
 	if skillPoints > 0:
-		att_speed_mult += value
+		if type in stats:
+			stats[type] += value
 		skillPoints -= 1
 		
 		return true
