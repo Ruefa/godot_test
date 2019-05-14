abc6ffcf1704adfdcdc7d2fcead74b1772511eb8
ruefa <ruef.alex@gmail.com>
2019-05-14 16:57:50 -0700

    added global method for loading json files loadJSON

--- Side Scroll/Constants.gd
+++ Side Scroll/Constants.gd
@@ -6,3 +6,13 @@ const GRAVITY = 500
 const ATT_SPEED = "att_speed"
 const ATT_DAMAGE = "att_damage"
 const PROJ = "projectile"
+
+func loadJSON():
+	var JsonFile = File.new()
+	JsonFile.open("res://JSON/skill_tree.json", JsonFile.READ)
+	
+	# parse json into text map
+	var JsonDict = JSON.parse(JsonFile.get_as_text()).result
+	JsonFile.close()
+	
+	return JsonDict
